f12600b3a5f56d3fe4e1a92f23e84f3c0d73185b
Kri <kristikapi@gmail.com>
2021-01-31 22:11:17 +0200

    krische v2

--- src/components/About.js
+++ src/components/About.js
@@ -9,9 +9,9 @@ const About = (props) => {
             </p>
             <p>Although I gained interest in programming fairly recently, I consider myself a fast and determined learner, which helped me gain a quick pace in my ongoing journey towards becoming a great developer.   
             </p>
-            <p>I used to be one of the best tennis players in the country, which has helped me gain exceptional time management skills, has consistenly tested my willpower and helped me grow. 
+            <p>I used to be one of the best tennis players in the country, which has helped me gain exceptional time management skills and has consistenly tested my willpower and devotion. 
             </p> 
-            <p> Being a well-rounded person is key to personal growth, which is why I am thankful for all the years spent training tennis professionally.</p>
+            <p> Being a well-rounded person is key to personal growth, which is why I am thankful for all the years spent training tennis professionally, as they have shaped me to become the disciplined and motivated person I am today.</p>
         </div>
     </div>
     )
